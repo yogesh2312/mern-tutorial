f0b6e9e10a51fac928b702900ae83aa31346f001
yogesh2312 <yogeshchikhale98@gmail.com>
2025-05-18 21:17:23 +0530

    casing of pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
-import Dashboard from './pages/dashboard';
-import Login from './pages/login';
-import Register from './pages/register';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Dashboard from './pages/Dashboard';
 import Header from './components/Header';
 
 
